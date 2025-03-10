0886bff5f550f4b2a4684ce098fd3cb77362d6ab
TeodorPetrovic <teodor.z.petrovic@gmail.com>
2025-03-10 15:50:48 +0100

    Configured Filter for Jwt and filter for OTP token

--- src/main/java/com/birp/chat_backend/services/UserService.java
+++ src/main/java/com/birp/chat_backend/services/UserService.java
@@ -1,6 +1,7 @@
 package com.birp.chat_backend.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,9 @@ public class UserService {
 
     public List<User> fetchAllUsers() {
         return this.userRepository.findAll();
+    }
+
+    public Optional<User> fetchUserById(int userId) {
+        return this.userRepository.findById(userId);
     }
 }
