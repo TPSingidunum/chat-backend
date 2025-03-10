eee63299ea15c089ecd8617ff9639222c674e8f1
TeodorPetrovic <teodor.z.petrovic@gmail.com>
2025-03-10 14:58:49 +0100

    Improved the SessionService

---
+++ src/main/java/com/birp/chat_backend/dtos/UserLoginDto.java
@@ -0,0 +1,16 @@
+package com.birp.chat_backend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginDto {
+    private String userId;
+    private String username;
+    private String email;
+}
