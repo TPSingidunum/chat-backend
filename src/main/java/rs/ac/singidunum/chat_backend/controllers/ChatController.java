91fb11a31e640dc2ea88e961ca20959eabb50d04
student <student@gmail.com>
2026-04-28 10:12:49 +0200

    Added fetch user endpoint

--- src/main/java/rs/ac/singidunum/chat_backend/controllers/ChatController.java
+++ src/main/java/rs/ac/singidunum/chat_backend/controllers/ChatController.java
@@ -1,11 +1,13 @@
 package rs.ac.singidunum.chat_backend.controllers;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import rs.ac.singidunum.chat_backend.dtos.chat.LoginRequest;
 import rs.ac.singidunum.chat_backend.dtos.chat.LoginResponse;
 import rs.ac.singidunum.chat_backend.websocket.ConnectedUsers;
@@ -23,15 +25,27 @@ public class ChatController {
     @SendToUser("/queue/auth")
     public LoginResponse login(LoginRequest loginRequest, @Header("simpSessionId") String sessionId) {
 
-        connectedUsers.register(sessionId,loginRequest.getUsername());
+        connectedUsers.register(sessionId, loginRequest.getUsername());
         broadcastConnectedUsers();
 
         return new LoginResponse(true,"You have successfully connected");
+    }
+
+    @MessageMapping("/users.connected")
+    @SendToUser("/queue/connected-users")
+    public List<String> getConnectedUsers() {
+        return connectedUsers.getConnectedUsers();
     }
 
     void broadcastConnectedUsers() {
         System.out.println("Sending user information");
         List<String> users = connectedUsers.getConnectedUsers();
         simpMessagingTemplate.convertAndSend("/topic/users", users);
+    }
+
+    @EventListener
+    public void onDisconnect(SessionDisconnectEvent event) {
+        connectedUsers.remove(event.getSessionId());
+        broadcastConnectedUsers();
     }
 }
